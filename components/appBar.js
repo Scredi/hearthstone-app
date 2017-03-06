ac34f44f9d097fe9abf518efded412c87f6575bc
Maxime Petronio <m.petronio91@gmail.com>
2017-03-06 18:46:43 +0100

    delete useless files

--- components/appBar.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import MuiAppBar from "material-ui/AppBar"
-import Link from "next/link"
-
-export default () => {
-    const title = <Link href="/"><a  style={{ color: "#fff", textDecoration: "none" }}>Hearthstone App</a></Link>
-    return <MuiAppBar title={title} style={{ color: "#fff" }}/>
-}
